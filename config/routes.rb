6386e1558e464476b173378525863ffbbf7ee0e0
tokyo3751 <tomizawa3751@gmail.com>
2022-07-02 14:51:01 +0000

    [Fix]ヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
+  get 'homes/about' => 'homes#about'
+
   resources :books
   resources :users, only:[:index,:show,:edit]
 end
