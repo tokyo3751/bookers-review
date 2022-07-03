744dbfa125a037995c530ee9fce3edf446faadea
tokyo3751 <tomizawa3751@gmail.com>
2022-07-03 08:20:07 +0000

    [Add]投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
+
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
   end
 
   def index
@@ -16,4 +24,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+   def book_params
+     params.require(:book).permit(:title,:comment)
+   end
 end
