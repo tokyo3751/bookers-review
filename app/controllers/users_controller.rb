55ea40183fc8b6bbe64f842c2637bfb763ab71b3
tokyo3751 <tomizawa3751@gmail.com>
2022-07-04 11:32:12 +0000

    [Add]User info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+
   def index
+    @users = User.all
+    @user = current_user
   end
 
   def edit
   end
+
+  private
+   def user_params
+     params.require(:user).permit(:name,:email,:image)
+   end
 end
