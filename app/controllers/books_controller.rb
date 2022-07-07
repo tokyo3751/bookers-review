dac38278e4c9ef7b7eb690c308bf084b8f9114ae
tokyo3751 <tomizawa3751@gmail.com>
2022-07-07 14:27:56 +0000

    [Add]bookのeditページのupdate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,17 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book == current_user
+      render :edit
+    else
+      redirect_to book_path(@book)
+    end
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
