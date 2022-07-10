967c2dd589ac42dcf62b6eb6b1ec22b3745bf54e
tokyo3751 <tomizawa3751@gmail.com>
2022-07-10 12:48:54 +0000

    [Fix]has_one_attachedの名前変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,6 @@ class UsersController < ApplicationController
 
   private
    def user_params
-     params.require(:user).permit(:name,:image,:introduction)
+     params.require(:user).permit(:name,:profile_image,:introduction)
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :books, dependent: :destroy
-    has_one_attached :image
+    has_one_attached :profile_image
 
     validates :name, length: { in: 2..20 } , uniqueness: true
     validates :introduction, length: { maximum: 50 }
 
-  def get_image
-    unless image.attached?
+  def get_profile_image
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+      profile_image
   end
 
 end
