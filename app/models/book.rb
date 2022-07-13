7a7ea5c5778173ae7c52bb0142c9c642081660c0
tokyo3751 <tomizawa3751@gmail.com>
2022-07-13 07:17:42 +0000

    [Add]いいね機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
   has_one_attached :profile_image
 
   validates :title, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'homes#top'
   devise_for :users
 
   get 'home/about' => 'homes#about'
 
-  resources :books
+  resources :books do
+    resource :favorites, only:[:create, :destroy]
+  end
   resources :users
 end
