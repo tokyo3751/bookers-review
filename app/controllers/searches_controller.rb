a55a80fe89646468dff1a1c94180d8f2a78977a5
tokyo3751 <tomizawa3751@gmail.com>
2022-07-25 15:25:31 +0000

    [Finish]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,13 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def search
     @range = params[:range]
-    
+    @word = params[:word]
+
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
-    else 
+    else
       @books = Book.looks(params[:search], params[:word])
     end
   end
