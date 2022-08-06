463d54f7d0c4280383735971b2dd6936b778db17
tokyo3751 <tomizawa3751@gmail.com>
2022-08-06 02:52:33 +0000

    [Finish]投稿数の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,8 @@ class UsersController < ApplicationController
     end
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
