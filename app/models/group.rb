1e162031f655d47fe276f080fdaa9450e7887d9c
tokyo3751 <tomizawa3751@gmail.com>
2022-08-19 10:24:57 +0000

    [Fix]モデルファイル

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,20 @@
 class Group < ApplicationRecord
-  has_many :group_users
-  has_many :users, through: :group_users
+  has_many :group_users, dependent: :destroy
+  has_many :users, through: :group_users, source: :user
 
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :image
+
+  def get_image
+    (image.attached?) ? image : "no_image.jpg"
+  end
+
+  def is_owned_by?(user)
+    owner_id == user.id
+  end
+
+  def includesUser?(user)
+    group_users.exists?(user_id: user.id)
+  end
 end
