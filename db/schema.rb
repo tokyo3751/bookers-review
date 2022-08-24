e78892f56ec96ad80a848dcf4a04db81ae74895d
tokyo3751 <tomizawa3751@gmail.com>
2022-08-24 12:44:11 +0000

    [Add]スターの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_10_072223) do
+ActiveRecord::Schema.define(version: 2022_08_24_123707) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_08_10_072223) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.float "star"
   end
 
   create_table "chats", force: :cascade do |t|
